5cfb302b5572a6a1f9c84751b98ca221ed0de6da
fishnoggin <masonblck@gmail.com>
2022-05-02 11:51:49 -0400

    contact page add

---
+++ scripts/updates.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", footerUpdate());
+
+
+function footerUpdate(){
+	var footer = document.getElementById("footer");
+	footer.innerHTML = "<div><span class='narrow'>mason black 2022</span></div>		<div><span class='narrow' style='margin-right: 20px;'><a href='../contact.html'>contact</a></span></div>"
+}
