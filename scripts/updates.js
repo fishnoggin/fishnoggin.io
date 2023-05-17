608f6bb0420663b9f12bfd8f014dfaab05b5fd6c
fishnoggin <masonblck@gmail.com>
2023-05-17 15:40:52 -0400

    edit

--- scripts/updates.js
+++ scripts/updates.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", footerUpdate());
 
 function footerUpdate(){
 	var footer = document.getElementById("footer");
-	footer.innerHTML = "<div><span class='narrow'>mason black 2022</span></div>\n" +
+	footer.innerHTML = "<div><span class='narrow'>mason black 2023</span></div>\n" +
 	"<div><span class='narrow' style='margin-right: 20px;'> \n" +
 	"<a href='../contact.html'>contact</a></span></div>";
 }
